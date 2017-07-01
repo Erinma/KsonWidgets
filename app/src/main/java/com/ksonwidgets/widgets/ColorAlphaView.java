d1e98d14ab0b98d66f91132ac9e30c346683f6ba
ksonCode <19655910@qq.com>
2017-07-02 01:01:47 +0800

    xmode相关view

---
+++ app/src/main/java/com/ksonwidgets/widgets/ColorAlphaView.java
@@ -0,0 +1,24 @@
+package com.ksonwidgets.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by kson on 2017/7/2.
+ */
+
+public class ColorAlphaView extends View {
+    public ColorAlphaView(Context context) {
+        super(context);
+    }
+
+    public ColorAlphaView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ColorAlphaView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/com/ksonwidgets/widgets/RadiusImageView.java
@@ -0,0 +1,24 @@
+package com.ksonwidgets.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by kson on 2017/7/2.
+ */
+
+public class RadiusImageView extends ImageView{
+    public RadiusImageView(Context context) {
+        super(context);
+    }
+
+    public RadiusImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RadiusImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
