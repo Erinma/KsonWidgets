7f9f54aa7b6ec6a8166c6b2bd32e684f755bc5fd
ksonCode <19655910@qq.com>
2017-07-01 17:12:49 +0800

    AutoBanner架构设计

--- app/src/main/java/com/ksonwidgets/activity/AutoBannerActivity.java
+++ app/src/main/java/com/ksonwidgets/activity/AutoBannerActivity.java
@@ -2,6 +2,8 @@ package com.ksonwidgets.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.ksonwidgets.R;
 import com.ksonwidgets.widgets.autobanner.AutoBanner;
@@ -9,23 +11,33 @@ import com.ksonwidgets.widgets.autobanner.AutoBanner;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AutoBannerActivity extends AppCompatActivity {
+public class AutoBannerActivity extends AppCompatActivity implements AutoBanner.AutoBannerListener {
 
-    private AutoBanner carouselImagesView;
+    private AutoBanner mAutoBanner;
     private List<String> mImgUrls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_carousel_images);
+        setContentView(R.layout.activity_auto_banner);
+
+        mAutoBanner = (AutoBanner) findViewById(R.id.auto_banner);
 
         mImgUrls = new ArrayList<>();
 
         mImgUrls.add("http://pic.58pic.com/58pic/13/61/00/61a58PICtPr_1024.jpg");
+        mImgUrls.add("http://pic.58pic.com/58pic/13/61/00/61a58PICtPr_1024.jpg");
+        mImgUrls.add("http://pic.58pic.com/58pic/13/61/00/61a58PICtPr_1024.jpg");
+        mImgUrls.add("http://pic.58pic.com/58pic/13/61/00/61a58PICtPr_1024.jpg");
 
-        carouselImagesView = (AutoBanner) findViewById(R.id.images);
 
-        carouselImagesView.displayADs(mImgUrls);
+        mAutoBanner.load(mImgUrls).setOnAutoBannerListener(this).display();
 
+
+    }
+
+    @Override
+    public void onClickListener(int postion) {
+        Toast.makeText(AutoBannerActivity.this, "点击了：" + postion + "位置", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/ksonwidgets/widgets/autobanner/AutoBanner.java
+++ app/src/main/java/com/ksonwidgets/widgets/autobanner/AutoBanner.java
@@ -1,11 +1,18 @@
 package com.ksonwidgets.widgets.autobanner;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
+import com.ksonwidgets.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,59 +22,142 @@ import java.util.List;
  */
 
 public class AutoBanner extends FrameLayout {
-    private List<String> mImgUrls = new ArrayList<>();
 
-    private List<ImageView> mDotView = new ArrayList<>();
     private Context mContext;
+    private int mIndicatorWidth, mIndicatorHeight, mIndicatorMargin;
+    private List<String> mImgUrls;
+    private List<View> mImageViews;
+    private List<ImageView> mIndicatorViews;
+    private LinearLayout mIndicatorLayout;
+    private ViewPager mViewPager;
+    private AutoBannerListener mAutoBannerListener;
+    private int count;
 
     public AutoBanner(Context context) {
-        super(context);
-        init(context);
+        this(context, null);
     }
 
     public AutoBanner(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
+        this(context, attrs, 0);
     }
 
     public AutoBanner(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs);
     }
 
-    private void init(Context context){
+    private void init(Context context, AttributeSet attrs) {
         this.mContext = context;
+        mImageViews = new ArrayList<>();
+        mImgUrls = new ArrayList<>();
+        mIndicatorViews = new ArrayList<>();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AutoBanner);
+        mIndicatorWidth = typedArray.getDimensionPixelSize(R.styleable.AutoBanner_indicator_width, 30);
+        mIndicatorHeight = typedArray.getDimensionPixelSize(R.styleable.AutoBanner_indicator_height, 30);
+        mIndicatorMargin = typedArray.getDimensionPixelSize(R.styleable.AutoBanner_indicator_margin, 10);
+
+        typedArray.recycle();
+
+        View view = LayoutInflater.from(context).inflate(R.layout.auto_banner_layout, this, true);
+        mViewPager = (ViewPager) view.findViewById(R.id.banner_viewpager);
+        mIndicatorLayout = (LinearLayout) view.findViewById(R.id.circleIndicator);
+
+
     }
 
     /**
-     * 展示广告位
+     * 设置图片源
      *
-     * @param urls 图片列表数据
+     * @param imgs
+     * @return
      */
-    public void displayADs(List<String> urls) {
-        FrameLayout layout = new FrameLayout(mContext);
-        this.mImgUrls = urls;
+    public AutoBanner load(List<String> imgs) {
+        this.mImgUrls = imgs;
+        this.count = mImgUrls.size();
+        return this;
+    }
 
-        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.WRAP_CONTENT);
-
-
-        ImageView imageView = new ImageView(mContext);
-
-        imageView.setLayoutParams(params);
-        Glide.with(mContext).load(urls.get(0)).into(imageView);
-
-
-        layout.addView(imageView);
-
-        addView(layout);
-
-
+    /**
+     * 加载和显示banner
+     *
+     * @return
+     */
+    public AutoBanner display() {
+        setImages(mImgUrls);
+        setIndicators();
+        return this;
+    }
 
 
+    public AutoBanner setAnimation() {
+
+        return this;
+
+    }
+
+
+    private void setImages(List<String> mImgUrls) {
+
+
+    }
+
+    private void setIndicators() {
+
+        mIndicatorViews.clear();
+        mIndicatorLayout.removeAllViews();
+        if (count < 2) {//一个view时不绘制指示器
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+
+            ImageView imageView = new ImageView(mContext);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(mIndicatorWidth, mIndicatorHeight);
+            layoutParams.leftMargin = mIndicatorMargin;
+            layoutParams.rightMargin = mIndicatorMargin;
+            imageView.setImageResource(R.drawable.indicator_unselect);
+
+            mIndicatorViews.add(imageView);
+
+            mIndicatorLayout.addView(imageView, layoutParams);
+
+
+        }
+
+
+    }
+
+    class ViewPagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+    }
+
+
+    public AutoBanner setOnAutoBannerListener(AutoBannerListener listener) {
+
+        this.mAutoBannerListener = listener;
+
+        return this;
+
+    }
+
+
+    public interface AutoBannerListener {
+        void onClickListener(int postion);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+        super.onLayout(changed, l, t, r, b);
 
     }
 }
