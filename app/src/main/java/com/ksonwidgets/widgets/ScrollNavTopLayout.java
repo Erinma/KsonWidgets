79b9b79cc26e009aebf88e1bc0529705cf627f22
ksonCode <19655910@qq.com>
2017-07-02 01:05:10 +0800

    scroll套各种布局，冲突解决，顶部导航栏滑动一段距离置顶

---
+++ app/src/main/java/com/ksonwidgets/widgets/ScrollNavTopLayout.java
@@ -0,0 +1,24 @@
+package com.ksonwidgets.widgets;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by kson on 2017/7/2.
+ */
+
+public class ScrollNavTopLayout extends LinearLayout {
+    public ScrollNavTopLayout(Context context) {
+        super(context);
+    }
+
+    public ScrollNavTopLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollNavTopLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
